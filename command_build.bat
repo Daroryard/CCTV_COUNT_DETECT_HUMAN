c24cee6758f074e7859c2d10587857a16e142d38
Daroryard <120617749+Daroryard@users.noreply.github.com>
2026-07-29 09:23:42 +0700

    Add files via upload

---
+++ command_build.bat
@@ -0,0 +1,28 @@
+@echo off
+echo ===================================================
+echo  Starting PyInstaller Build Process for CCTV-YOLO
+echo ===================================================
+
+cd source_code
+
+
+py -m PyInstaller --onefile --windowed --icon=cctv-camera.ico --distpath=..\dist --name=CCTV_AI_Analytics cctv_app.py
+
+if %ERRORLEVEL% EQU 0 (
+    echo.
+    echo [SUCCESS] Build completed! Copying resource files to 'dist' folder...
+    
+    copy "yolov8s.pt" "..\dist\"
+    copy "yolov8m.pt" "..\dist\"
+    copy "multi_config.json" "..\dist\"
+    copy "test.mp4" "..\dist\"
+    copy "test1.mp4" "..\dist\"
+    
+    echo [SUCCESS] All files are ready in the main 'dist' folder!
+) else (
+    echo.
+    echo [ERROR] Build failed.
+)
+
+echo.
+pause
